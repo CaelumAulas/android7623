aba01e347f99c9d5a315409aa9c1754f9fa8e040
Matheus Brandino <m.v.brandino@gmail.com>
2018-07-09 15:53:13 -0300

    formulario d ealunos com material design - 3.1

---
+++ app/src/main/java/br/com/caelum/listacontatos/FormularioActivity.java
@@ -0,0 +1,13 @@
+package br.com.caelum.listacontatos;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+    }
+}
